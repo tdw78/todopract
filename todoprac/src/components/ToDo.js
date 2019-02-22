3ac4670efd19d3a535d7d1ea7169e5bb15af83e7
Tim Weiss <tdweiss1978@gmail.com>
2019-02-22 12:48:36 -0800

    Added ToDo List

---
+++ todoprac/src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render(){
+    return(
+      <div>
+        <li>Item 11</li>
+      </div>
+    );
+  }
+}
+
+export default ToDo;
